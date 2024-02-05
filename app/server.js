e372dcb1901deeb4528d0fafa8bf3d3fdf9a27bd
vaibhav-dawane <vaibhavdawane07@gmail.com>
2024-02-05 23:16:30 +0530

    e

--- app/server.js
+++ app/server.js
@@ -21,10 +21,10 @@ app.get('/profile-picture', function (req, res) {
 });
 
 // use when starting application locally with node command
-let mongoUrlLocal = "mongodb://admin:password@mongo:27017"; 
+let mongoUrlLocal = "mongodb://admin:password@localhost:27017"; 
 
 // use when starting application as a separate docker container
-let mongoUrlDocker = "mongodb://admin:password@mongo:27017";
+let mongoUrlDocker = "mongodb://admin:password@localhost:27017";
 
 // use when starting application as docker container, part of docker-compose
 let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
